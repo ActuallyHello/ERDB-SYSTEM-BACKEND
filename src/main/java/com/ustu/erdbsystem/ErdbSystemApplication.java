daa8b4b2c591c1a08a76f835ba0227eade72e8b8
HappyMq <happy.xmas1@mail.ru>
2023-09-27 20:47:34 +0300

    initialized spring project

---
+++ src/main/java/com/ustu/erdbsystem/ErdbSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ustu.erdbsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErdbSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErdbSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ustu/erdbsystem/ErdbSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ustu.erdbsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErdbSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
