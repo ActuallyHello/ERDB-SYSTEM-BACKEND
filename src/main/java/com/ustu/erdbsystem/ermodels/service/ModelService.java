58ca4d2ea839386b49ad6207149bb0b95b2e73fa
HappyMq <happy.xmas1@mail.ru>
2023-10-03 23:50:55 +0300

    changed return type and added creation method

--- src/main/java/com/ustu/erdbsystem/ermodels/service/ModelService.java
+++ src/main/java/com/ustu/erdbsystem/ermodels/service/ModelService.java
@@ -1,17 +1,16 @@
 package com.ustu.erdbsystem.ermodels.service;
 
-import com.ustu.erdbsystem.ermodels.dto.ModelDetailDTO;
-import com.ustu.erdbsystem.ermodels.dto.ModelPreviewDTO;
+import com.ustu.erdbsystem.ermodels.api.dto.CreateModelDTO;
 import com.ustu.erdbsystem.ermodels.store.models.Model;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ModelService {
-    List<ModelPreviewDTO> getAll();
-    List<ModelPreviewDTO> getAll(List<Long> idList);
+    List<Model> getAll();
+    List<Model> getAll(List<Long> idList);
 
-    Optional<ModelDetailDTO> getById(Long id);
+    Optional<Model> getById(Long id);
 
-
+    Long create(CreateModelDTO createModelDTO);
 }
--- src/main/java/com/ustu/erdbsystem/ermodels/service/impl/ModelServiceImpl.java
+++ src/main/java/com/ustu/erdbsystem/ermodels/service/impl/ModelServiceImpl.java
@@ -1,10 +1,14 @@
 package com.ustu.erdbsystem.ermodels.service.impl;
 
-import com.ustu.erdbsystem.ermodels.dto.ModelDetailDTO;
-import com.ustu.erdbsystem.ermodels.dto.ModelPreviewDTO;
-import com.ustu.erdbsystem.ermodels.factory.impl.ModelDetailDTOFactory;
-import com.ustu.erdbsystem.ermodels.factory.impl.ModelPreviewDTOFactory;
+import com.ustu.erdbsystem.ermodels.api.dto.CreateModelDTO;
+import com.ustu.erdbsystem.ermodels.api.mapper.impl.ModelDetailDTOMapper;
+import com.ustu.erdbsystem.ermodels.api.mapper.impl.ModelPreviewDTOMapper;
 import com.ustu.erdbsystem.ermodels.service.ModelService;
+import com.ustu.erdbsystem.ermodels.store.models.Attribute;
+import com.ustu.erdbsystem.ermodels.store.models.Model;
+import com.ustu.erdbsystem.ermodels.store.models.ModelEntity;
+import com.ustu.erdbsystem.ermodels.store.repos.AttributeRepo;
+import com.ustu.erdbsystem.ermodels.store.repos.ModelEntityRepo;
 import com.ustu.erdbsystem.ermodels.store.repos.ModelRepo;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
@@ -20,28 +24,62 @@ import java.util.Optional;
 public class ModelServiceImpl implements ModelService {
 
     private ModelRepo modelRepo;
-    private ModelPreviewDTOFactory modelPreviewDTOFactory;
-    private ModelDetailDTOFactory modelDetailDTOFactory;
+    private ModelEntityRepo modelEntityRepo;
+    private AttributeRepo attributeRepo;
+
+    private ModelPreviewDTOMapper modelPreviewDTOFactory;
+    private ModelDetailDTOMapper modelDetailDTOFactory;
 
     @Override
     @Transactional
-    public List<ModelPreviewDTO> getAll() {
-        return modelRepo.findAll().stream()
-                .map(modelPreviewDTOFactory::makeDTO)
-                .toList();
+    public List<Model> getAll() {
+        return modelRepo.findAll().stream().toList();
     }
 
     @Override
     @Transactional
-    public List<ModelPreviewDTO> getAll(List<Long> idList) {
-        return modelRepo.findByIdIn(idList).stream()
-                .map(modelPreviewDTOFactory::makeDTO)
-                .toList();
+    public List<Model> getAll(List<Long> idList) {
+        return modelRepo.findByIdIn(idList).stream().toList();
     }
 
     @Override
     @Transactional
-    public Optional<ModelDetailDTO> getById(Long id) {
-        return modelRepo.findById(id).map(modelDetailDTOFactory::makeDTO);
+    public Optional<Model> getById(Long id) {
+        return modelRepo.findById(id);
+    }
+
+    /**
+     * CREATE A MODEL WITH ENTITIES AND THEIR ATTRIBUTES
+     * Create model object, then iterate through model entities
+     *  and then iterate through entity attributes
+     * @param createModelDTO - dto from client
+     * @return - model's id
+     */
+    @Override
+    @Transactional
+    public Long create(CreateModelDTO createModelDTO) {
+        Model model = Model.builder()
+                .title(createModelDTO.title())
+                .description(createModelDTO.description())
+                .topic(createModelDTO.topic())
+                .person(createModelDTO.person())
+                .isTaskResult(createModelDTO.isTaskResult())
+                .build();
+        for (var modelEntityDTO : createModelDTO.modelEntityDTOList()) {
+            ModelEntity modelEntity = ModelEntity.builder()
+                            .title(modelEntityDTO.title())
+                            .model(model)
+                            .build();
+            for (var attributeDTO : modelEntityDTO.attributeDTOList()) {
+                Attribute attribute = Attribute.builder()
+                        .title(attributeDTO.title())
+                        .attributeType(attributeDTO.attributeType())
+                        .modelEntity(modelEntity)
+                        .build();
+                modelEntity.getAttributeList().add(attribute);
+            }
+            model.getModelEntityList().add(modelEntity);
+        }
+        return modelRepo.saveAndFlush(model).getId();
     }
 }
