c1b2ec8c0e7c24165582e4e3103227018b7008cd
HappyMq <happy.xmas1@mail.ru>
2023-10-23 23:02:48 +0300

    optimize queries

--- src/main/java/com/ustu/erdbsystem/ermodels/store/repos/ModelEntityRepo.java
+++ src/main/java/com/ustu/erdbsystem/ermodels/store/repos/ModelEntityRepo.java
@@ -1,9 +1,21 @@
 package com.ustu.erdbsystem.ermodels.store.repos;
 
+import com.ustu.erdbsystem.ermodels.store.models.Model;
 import com.ustu.erdbsystem.ermodels.store.models.ModelEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ModelEntityRepo extends JpaRepository<ModelEntity, Long> {
+
+    @Query("""
+           SELECT e FROM ModelEntity e
+           INNER JOIN FETCH e.attributeList
+           WHERE e.model = :model
+           """
+    )
+    List<ModelEntity> getAllByModel(Model model);
 }
--- src/main/java/com/ustu/erdbsystem/ermodels/store/repos/ModelRepo.java
+++ src/main/java/com/ustu/erdbsystem/ermodels/store/repos/ModelRepo.java
@@ -2,11 +2,17 @@ package com.ustu.erdbsystem.ermodels.store.repos;
 
 import com.ustu.erdbsystem.ermodels.store.models.Model;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ModelRepo extends JpaRepository<Model, Long> {
+
+    @Override
+    @Query("SELECT m FROM Model m INNER JOIN FETCH m.person")
+    List<Model> findAll();
+
     List<Model> findByIdIn(List<Long> idList);
 }
