af271e24679edea4e8e83f0ce46dda91c8cf553e
HappyMq <happy.xmas1@mail.ru>
2023-10-25 22:51:04 +0300

    setup user controller

---
+++ src/main/java/com/ustu/erdbsystem/persons/api/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.ustu.erdbsystem.persons.api.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+public class UserController {
+}
