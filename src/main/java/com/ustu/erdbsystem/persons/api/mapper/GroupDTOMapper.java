71727b1edf2c8b215e5deab81aea8c592350b5f7
HappyMq <happy.xmas1@mail.ru>
2023-10-25 22:49:49 +0300

    created group dto

---
+++ src/main/java/com/ustu/erdbsystem/persons/api/mapper/GroupDTOMapper.java
@@ -0,0 +1,24 @@
+package com.ustu.erdbsystem.persons.api.mapper;
+
+import com.ustu.erdbsystem.persons.api.dto.GroupDTO;
+import com.ustu.erdbsystem.persons.store.models.Group;
+
+public class GroupDTOMapper {
+    public static GroupDTO makeDTO(Group group) {
+        if (group == null) throw  new IllegalArgumentException("group is null");
+        return GroupDTO.builder()
+                .id(group.getId())
+                .title(group.getTitle())
+                .build();
+    }
+
+    public static Group fromDTO(GroupDTO groupDTO) {
+        if (groupDTO == null) throw  new IllegalArgumentException("groupDTO is null");
+        return Group.builder()
+                .id(groupDTO.getId())
+                .title(groupDTO.getTitle())
+                .isActive(groupDTO.getIsActive())
+                .build();
+    }
+
+}
