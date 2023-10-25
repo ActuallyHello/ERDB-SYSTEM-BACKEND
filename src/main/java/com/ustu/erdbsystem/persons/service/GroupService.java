f18e661d299d1be2bbee59853c417d924dc2e243
HappyMq <happy.xmas1@mail.ru>
2023-10-25 22:43:29 +0300

    group service interface

--- src/main/java/com/ustu/erdbsystem/persons/service/GroupService.java
+++ src/main/java/com/ustu/erdbsystem/persons/service/GroupService.java
@@ -1,4 +1,21 @@
 package com.ustu.erdbsystem.persons.service;
 
+import com.ustu.erdbsystem.persons.api.dto.GroupDTO;
+import com.ustu.erdbsystem.persons.store.models.Group;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface GroupService {
+    List<Group> getAll();
+
+    List<Group> getAll(Boolean isActive);
+
+    Optional<Group> getById(Long id);
+
+    Group create(GroupDTO groupDTO);
+
+    void delete(Group group);
+
+    Group update(Group groupNew);
 }
