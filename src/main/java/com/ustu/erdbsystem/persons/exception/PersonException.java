3c1c7e7c6a69ea3da4c39cb2ee066703f5099fdd
HappyMq <happy.xmas1@mail.ru>
2023-10-25 22:48:28 +0300

    common exception for person controller

--- src/main/java/com/ustu/erdbsystem/persons/exception/PersonException.java
+++ src/main/java/com/ustu/erdbsystem/persons/exception/PersonException.java
@@ -1,4 +1,14 @@
 package com.ustu.erdbsystem.persons.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@Getter
 public class PersonException {
+    private final String message;
+    private final String causeClass;
+    private final HttpStatus httpStatus;
+    private final int httpStatusCode;
 }
--- src/main/java/com/ustu/erdbsystem/persons/exception/PersonExceptionHandler.java
+++ src/main/java/com/ustu/erdbsystem/persons/exception/PersonExceptionHandler.java
@@ -1,4 +1,28 @@
 package com.ustu.erdbsystem.persons.exception;
 
+import com.ustu.erdbsystem.ermodels.exception.ModelException;
+import com.ustu.erdbsystem.persons.exception.response.PersonNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class PersonExceptionHandler {
+
+    @ExceptionHandler(
+            value = {
+                    PersonNotFoundException.class
+            }
+    )
+    public ResponseEntity<Object> handlePersonNotFoundException(
+            PersonNotFoundException personNotFoundException) {
+        var personException = new PersonException(
+                personNotFoundException.getMessage(),
+                personNotFoundException.getClass().getSimpleName(),
+                HttpStatus.NOT_FOUND,
+                HttpStatus.NOT_FOUND.value()
+        );
+        return new ResponseEntity<>(personException, HttpStatus.NOT_FOUND);
+    }
 }
