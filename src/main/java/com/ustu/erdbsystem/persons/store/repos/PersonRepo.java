f9e8ccf5f041ca386c67ea9323cbde023aec5a27
HappyMq <happy.xmas1@mail.ru>
2023-10-25 22:47:45 +0300

    refactor person repo

--- src/main/java/com/ustu/erdbsystem/persons/store/repos/PersonRepo.java
+++ src/main/java/com/ustu/erdbsystem/persons/store/repos/PersonRepo.java
@@ -1,9 +1,14 @@
 package com.ustu.erdbsystem.persons.store.repos;
 
 import com.ustu.erdbsystem.persons.store.models.Person;
+import com.ustu.erdbsystem.persons.store.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PersonRepo extends JpaRepository<Person, Long> {
+
+    Optional<Person> findByUser(User user);
 }
