98fc12921be1f883b441616c2bce0838f2f0b3fd
HappyMq <happy.xmas1@mail.ru>
2023-10-25 22:48:58 +0300

    created teacher service

--- src/main/java/com/ustu/erdbsystem/persons/service/TeacherService.java
+++ src/main/java/com/ustu/erdbsystem/persons/service/TeacherService.java
@@ -1,4 +1,22 @@
 package com.ustu.erdbsystem.persons.service;
 
+import com.ustu.erdbsystem.persons.store.models.Person;
+import com.ustu.erdbsystem.persons.store.models.Position;
+import com.ustu.erdbsystem.persons.store.models.Teacher;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface TeacherService {
+    List<Teacher> getAll();
+
+    Optional<Teacher> getById(Long id);
+
+    Optional<Teacher> getByPerson(Person person);
+
+    Teacher create(Person person, Position position);
+
+    void delete(Teacher teacher);
+
+    Teacher update(Teacher teacherNew);
 }
---
+++ src/main/java/com/ustu/erdbsystem/persons/service/impl/TeacherServiceImpl.java
@@ -0,0 +1,90 @@
+package com.ustu.erdbsystem.persons.service.impl;
+
+import com.ustu.erdbsystem.persons.exception.service.TeacherCreationException;
+import com.ustu.erdbsystem.persons.exception.service.TeacherDeleteException;
+import com.ustu.erdbsystem.persons.service.TeacherService;
+import com.ustu.erdbsystem.persons.store.models.Person;
+import com.ustu.erdbsystem.persons.store.models.Position;
+import com.ustu.erdbsystem.persons.store.models.Teacher;
+import com.ustu.erdbsystem.persons.store.repos.TeacherRepo;
+import jakarta.persistence.PersistenceException;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@AllArgsConstructor
+@Slf4j
+@Service
+public class TeacherServiceImpl implements TeacherService {
+
+    private TeacherRepo teacherRepo;
+
+    @Override
+    @Transactional
+    public List<Teacher> getAll() {
+        var teacherList = teacherRepo.findAll();
+        log.info("GET TEACHER ({})", teacherList.size());
+        return teacherList;
+    }
+
+    @Override
+    @Transactional
+    public Optional<Teacher> getById(Long id) {
+        var teacher = teacherRepo.findById(id);
+        log.info("GET TEACHER WITH ID={}", id);
+        return teacher;
+    }
+
+    @Override
+    public Optional<Teacher> getByPerson(Person person) {
+        var teacher = teacherRepo.findByPerson(person);
+        log.info("GET TEACHER BY PERSON WITH ID={}", person.getId());
+        return teacher;
+    }
+
+    @Override
+    @Transactional
+    public Teacher create(Person person, Position position) {
+        var teacher = Teacher.builder()
+                .person(person)
+                .build();
+        position.addTeacher(teacher);
+        try {
+            teacher = teacherRepo.saveAndFlush(teacher);
+            log.info("CREATE TEACHER WITH ID={}", teacher.getId());
+            return teacher;
+        } catch (PersistenceException exception) {
+            log.error("CANNOT CREATE TEACHER: {}", exception.getMessage());
+            throw new TeacherCreationException(exception.getMessage(), exception);
+        }
+    }
+
+    @Override
+    @Transactional
+    public void delete(Teacher teacher) {
+        try {
+            teacherRepo.delete(teacher);
+            log.info("TEACHER WITH ID={} WAS DELETED", teacher.getId());
+        } catch (PersistenceException exception) {
+            log.error("CANNOT DELETE TEACHER: {}", exception.getMessage());
+            throw new TeacherDeleteException(exception.getMessage(), exception);
+        }
+    }
+
+    @Override
+    @Transactional
+    public Teacher update(Teacher teacherNew) {
+        try {
+            var teacher = teacherRepo.saveAndFlush(teacherNew);
+            log.info("TEACHER WITH ID={} WAS UPDATED", teacher.getId());
+            return teacher;
+        } catch (PersistenceException exception) {
+            log.error("CANNOT UPDATE TEACHER: {}", exception.getMessage());
+            throw new TeacherDeleteException(exception.getMessage(), exception);
+        }
+    }
+}
