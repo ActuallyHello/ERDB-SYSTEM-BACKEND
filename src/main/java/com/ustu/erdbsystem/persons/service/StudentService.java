3dada31745b96dbdb8f5f6568ede9eb88ac3a521
HappyMq <happy.xmas1@mail.ru>
2023-10-27 20:29:03 +0300

    added self explained methods in service

--- src/main/java/com/ustu/erdbsystem/persons/service/StudentService.java
+++ src/main/java/com/ustu/erdbsystem/persons/service/StudentService.java
@@ -4,12 +4,18 @@ import com.ustu.erdbsystem.persons.store.models.Group;
 import com.ustu.erdbsystem.persons.store.models.Person;
 import com.ustu.erdbsystem.persons.store.models.Student;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StudentService {
+
+    List<Student> getAllByGroupIdWithPerson(Long groupId);
+
     Optional<Student> getById(Long id);
 
-    Optional<Student> getByPerson(Person person);
+    Optional<Student> getByIdWithPersonAndGroup(Long id);
+
+    Optional<Student> getByPersonIdWithGroup(Long personId);
 
     Student create(Person person, Group group);
 
