93c617b2a8d7affe7db5d11d1e84e6b0cb3cd11b
HappyMq <happy.xmas1@mail.ru>
2023-10-27 20:26:09 +0300

    added person dto mapper for creation dto

--- src/main/java/com/ustu/erdbsystem/persons/api/mapper/PersonDTOMapper.java
+++ src/main/java/com/ustu/erdbsystem/persons/api/mapper/PersonDTOMapper.java
@@ -1,7 +1,9 @@
 package com.ustu.erdbsystem.persons.api.mapper;
 
 import com.ustu.erdbsystem.persons.api.dto.PersonDTO;
+import com.ustu.erdbsystem.persons.api.dto.request.CreatePersonRequestDTO;
 import com.ustu.erdbsystem.persons.store.models.Person;
+import com.ustu.erdbsystem.persons.store.models.enums.PersonType;
 
 import java.util.Objects;
 
@@ -14,6 +16,16 @@ public class PersonDTOMapper {
                 .lastName(person.getLastName())
                 .middleName(person.getMiddleName())
                 .personType(person.getPersonType())
+                .build();
+    }
+
+    public static PersonDTO makeDTO(CreatePersonRequestDTO createPersonRequestDTO) {
+        if (createPersonRequestDTO == null) throw new IllegalArgumentException("createPersonRequestDTO is null!");
+        return PersonDTO.builder()
+                .firstName(createPersonRequestDTO.getFirstName())
+                .lastName(createPersonRequestDTO.getLastName())
+                .middleName(createPersonRequestDTO.getMiddleName())
+                .personType(PersonType.fromString(createPersonRequestDTO.getPersonType()))
                 .build();
     }
 
