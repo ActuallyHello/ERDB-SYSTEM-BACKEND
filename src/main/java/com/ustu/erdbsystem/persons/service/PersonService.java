70fe8fc2187930610b08ba32d817522a9139fd11
HappyMq <happy.xmas1@mail.ru>
2023-10-27 20:30:22 +0300

    refactor person service

--- src/main/java/com/ustu/erdbsystem/persons/service/PersonService.java
+++ src/main/java/com/ustu/erdbsystem/persons/service/PersonService.java
@@ -10,6 +10,9 @@ import java.util.List;
 import java.util.Optional;
 
 public interface PersonService {
+
+    List<Person> getAll();
+
     Optional<Person> getById(Long id);
 
     Optional<Person> getByUser(User user);
