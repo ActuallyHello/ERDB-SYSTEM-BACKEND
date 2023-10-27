3f879d58cbbb969c3830a10122c2c1f99f877658
HappyMq <happy.xmas1@mail.ru>
2023-10-27 20:24:48 +0300

    student exception

---
+++ src/main/java/com/ustu/erdbsystem/persons/exception/response/StudentNotFoundException.java
@@ -0,0 +1,11 @@
+package com.ustu.erdbsystem.persons.exception.response;
+
+public class StudentNotFoundException extends RuntimeException {
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+
+    public StudentNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
