a6d1ba0318e29f6b7679cb0f227cc443be129fd1
HappyMq <happy.xmas1@mail.ru>
2023-11-21 23:52:12 +0300

    optimize query

--- src/main/java/com/ustu/erdbsystem/ermodels/store/repos/ModelRepo.java
+++ src/main/java/com/ustu/erdbsystem/ermodels/store/repos/ModelRepo.java
@@ -1,8 +1,9 @@
 package com.ustu.erdbsystem.ermodels.store.repos;
 
 import com.ustu.erdbsystem.ermodels.store.models.Model;
+import com.ustu.erdbsystem.persons.store.models.Person;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,18 +11,6 @@ import java.util.List;
 @Repository
 public interface ModelRepo extends JpaRepository<Model, Long> {
 
-    @Query("""
-            SELECT m FROM Model m
-                INNER JOIN FETCH m.person
-            """)
-    List<Model> findAllWithPerson();
-
-    @Query("""
-            SELECT m FROM Model m
-                INNER JOIN FETCH m.person
-            WHERE m.id IN :idList
-            """)
-    List<Model> findAllByIdInRangeWithPerson(List<Long> idList);
-
-    List<Model> findByIdIn(List<Long> idList);
+    List<Model> findAllByPerson(Person person);
+    List<Model> findAllBy(Pageable pageable);
 }
--- src/main/java/com/ustu/erdbsystem/persons/store/repos/PersonRepo.java
+++ src/main/java/com/ustu/erdbsystem/persons/store/repos/PersonRepo.java
@@ -3,6 +3,7 @@ package com.ustu.erdbsystem.persons.store.repos;
 import com.ustu.erdbsystem.persons.store.models.Person;
 import com.ustu.erdbsystem.persons.store.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,4 +12,11 @@ import java.util.Optional;
 public interface PersonRepo extends JpaRepository<Person, Long> {
 
     Optional<Person> findByUser(User user);
+
+    @Query("""
+            select p from Person p
+                left join fetch p.modelList
+            where p.id = :id
+            """)
+    Optional<Person> findByIdWithModels(Long id);
 }
