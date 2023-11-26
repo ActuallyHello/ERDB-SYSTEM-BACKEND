4210fe88a7365407f391d34004c3eaadc338ea25
HappyMq <happy.xmas1@mail.ru>
2023-11-27 00:33:49 +0300

    added fromString and getValue

--- src/main/java/com/ustu/erdbsystem/tasks/store/models/enums/Complexity.java
+++ src/main/java/com/ustu/erdbsystem/tasks/store/models/enums/Complexity.java
@@ -1,5 +1,7 @@
 package com.ustu.erdbsystem.tasks.store.models.enums;
 
+import com.ustu.erdbsystem.ermodels.exception.validation.EnumValueException;
+import com.ustu.erdbsystem.persons.store.models.enums.PersonType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,4 +11,17 @@ public enum Complexity {
     EASY("EASY"), NORMAL("NORMAL"), DIFFICULT("DIFFICULT");
 
     private final String complexity;
+
+    public static Complexity fromString(String complexity) {
+        for (var c : Complexity.values()) {
+            if (c.complexity.equalsIgnoreCase(complexity)) {
+                return c;
+            }
+        }
+        throw new EnumValueException("No such enum Complexity with value '%s'! [ValidationException]".formatted(complexity));
+    }
+
+    public String getValue() {
+        return complexity;
+    }
 }
