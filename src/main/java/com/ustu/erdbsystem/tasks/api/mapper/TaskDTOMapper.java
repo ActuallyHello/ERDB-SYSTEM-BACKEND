04a5a69bd15818e043dd22b081e205ac1bb7f479
HappyMq <happy.xmas1@mail.ru>
2023-11-27 00:40:48 +0300

    created taskdto mapper

---
+++ src/main/java/com/ustu/erdbsystem/tasks/api/mapper/TaskDTOMapper.java
@@ -0,0 +1,39 @@
+package com.ustu.erdbsystem.tasks.api.mapper;
+
+import com.ustu.erdbsystem.tasks.api.dtos.TaskDTO;
+import com.ustu.erdbsystem.tasks.api.dtos.request.CreateTaskRequestDTO;
+import com.ustu.erdbsystem.tasks.store.models.Task;
+import com.ustu.erdbsystem.tasks.store.models.enums.Complexity;
+
+public class TaskDTOMapper {
+
+    public static TaskDTO makeDTO(Task task) {
+        return TaskDTO.builder()
+                .id(task.getId())
+                .title(task.getTitle())
+                .description(task.getDescription())
+                .createdAt(task.getCreatedAt())
+                .updatedAt(task.getUpdatedAt())
+                .complexity(task.getComplexity())
+                .testDataAmount(task.getTestDataAmount())
+                .build();
+    }
+
+    public static TaskDTO makeDTO(CreateTaskRequestDTO createTaskRequestDTO) {
+        return TaskDTO.builder()
+                .title(createTaskRequestDTO.getTitle())
+                .description(createTaskRequestDTO.getDescription())
+                .complexity(Complexity.fromString(createTaskRequestDTO.getComplexity()))
+                .testDataAmount(createTaskRequestDTO.getTestDataAmount())
+                .build();
+    }
+
+    public static Task fromDTO(TaskDTO taskDTO) {
+        return Task.builder()
+                .title(taskDTO.getTitle())
+                .description(taskDTO.getDescription())
+                .complexity(taskDTO.getComplexity())
+                .testDataAmount(taskDTO.getTestDataAmount())
+                .build();
+    }
+}
