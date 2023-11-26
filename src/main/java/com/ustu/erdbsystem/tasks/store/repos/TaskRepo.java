91f12e141f4588eed7786eb16cdf7c32ef5b8230
HappyMq <happy.xmas1@mail.ru>
2023-11-27 00:38:21 +0300

    added queries

--- src/main/java/com/ustu/erdbsystem/tasks/store/repos/TaskRepo.java
+++ src/main/java/com/ustu/erdbsystem/tasks/store/repos/TaskRepo.java
@@ -2,6 +2,26 @@ package com.ustu.erdbsystem.tasks.store.repos;
 
 import com.ustu.erdbsystem.tasks.store.models.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface TaskRepo extends JpaRepository<Task, Long> {
+
+    @Query("""
+            select task from Task task
+                inner join fetch task.teacher teacher
+                inner join fetch teacher.person
+                inner join fetch teacher.position
+            """)
+    List<Task> findAllWithTeachers();
+
+    @Query("""
+            select task from Task task
+                inner join fetch task.denormalizeModelList
+            where task.id = :id
+            """
+    )
+    Optional<Task> findByIdWithDenormalizeModel(Long id);
 }
