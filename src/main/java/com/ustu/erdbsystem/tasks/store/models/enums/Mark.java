e4c37222299c294e94b07dad1462c2d8efdf1680
HappyMq <happy.xmas1@mail.ru>
2023-11-27 00:35:47 +0300

    added fromString and getValue

--- src/main/java/com/ustu/erdbsystem/tasks/store/models/enums/Mark.java
+++ src/main/java/com/ustu/erdbsystem/tasks/store/models/enums/Mark.java
@@ -1,5 +1,7 @@
 package com.ustu.erdbsystem.tasks.store.models.enums;
 
+import com.ustu.erdbsystem.ermodels.exception.validation.EnumValueException;
+import com.ustu.erdbsystem.persons.store.models.enums.PersonType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,4 +11,17 @@ public enum Mark {
     EXCELLENT("5"), BAD("2"), SATISFACTORY("3"), GOOD("4");
 
     private final String mark;
+
+    public static Mark fromString(String mark) {
+        for (var m : Mark.values()) {
+            if (m.mark.equalsIgnoreCase(mark)) {
+                return m;
+            }
+        }
+        throw new EnumValueException("No such enum PersonType with value '%s'! [ValidationException]".formatted(mark));
+    }
+
+    public String getValue() {
+        return mark;
+    }
 }
