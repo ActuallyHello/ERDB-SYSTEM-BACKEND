40e2112228cea21f7f47c7c77704718319c3a4b5
HappyMq <happy.xmas1@mail.ru>
2023-11-30 23:18:16 +0300

    added findByModelIdWithTasks

--- src/main/java/com/ustu/erdbsystem/tasks/store/repos/DenormalizeModelRepo.java
+++ src/main/java/com/ustu/erdbsystem/tasks/store/repos/DenormalizeModelRepo.java
@@ -2,8 +2,18 @@ package com.ustu.erdbsystem.tasks.store.repos;
 
 import com.ustu.erdbsystem.tasks.store.models.DenormalizeModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface DenormalizeModelRepo extends JpaRepository<DenormalizeModel, Long> {
+
+    @Query("""
+            select denormalizeModel from DenormalizeModel denormalizeModel
+                inner join fetch denormalizeModel.taskList
+            where denormalizeModel.model.id = :modelId
+            """)
+    Optional<DenormalizeModel> findByModelIdWithTasks(Long modelId);
 }
