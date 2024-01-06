e5768b2f76a1fd792864f28d2f4f5a55574f18e3
HappyMq <happy.xmas1@mail.ru>
2024-01-06 16:10:49 +0300

    refactor controller

--- src/main/java/com/ustu/erdbsystem/ermodels/api/controller/ModelController.java
+++ src/main/java/com/ustu/erdbsystem/ermodels/api/controller/ModelController.java
@@ -12,7 +12,11 @@ import com.ustu.erdbsystem.ermodels.exception.response.ModelServerException;
 import com.ustu.erdbsystem.ermodels.exception.response.ModelNotFoundException;
 import com.ustu.erdbsystem.ermodels.exception.service.ModelCreationException;
 import com.ustu.erdbsystem.ermodels.exception.service.ModelDeleteException;
+import com.ustu.erdbsystem.ermodels.exception.service.ModelEntityCreationException;
+import com.ustu.erdbsystem.ermodels.exception.service.RelationCreationException;
+import com.ustu.erdbsystem.ermodels.exception.service.RelationDeleteException;
 import com.ustu.erdbsystem.ermodels.service.ModelService;
+import com.ustu.erdbsystem.ermodels.service.facade.ModelEntityRelationFacade;
 import com.ustu.erdbsystem.external.TestDataLoader;
 import com.ustu.erdbsystem.external.exception.UploadTestDataException;
 import com.ustu.erdbsystem.persons.api.mapper.PersonDTOMapper;
@@ -24,6 +28,8 @@ import com.ustu.erdbsystem.tasks.service.ResultService;
 import com.ustu.erdbsystem.tasks.service.TaskService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -50,6 +56,8 @@ public class ModelController {
     private final TaskService taskService;
     private final TestDataLoader testDataLoader;
 
+    private final ModelEntityRelationFacade modelEntityRelationFacade;
+
     private final static String BY_ID = "/{id}";
     private final static String BY_PERSON_ID = "/persons/{personId}";
     private final static String UPLOAD_TEST_DATA_TO_MODEL = "/upload-test-data/{id}";
@@ -61,10 +69,11 @@ public class ModelController {
             @RequestParam(required = false) Boolean includeStudents,
             @RequestParam(required = false) Boolean includeTaskResults) {
         page = page == null ? 0 : page;
-        size = size == null ? 20 : size;
+        size = size == null ? 100 : size;
+        var pageable = PageRequest.of(page, size, Sort.by("id", "title"));
         if (includeStudents == null) includeStudents = false;
         if (includeTaskResults == null) includeTaskResults = false;
-        var modelList = modelService.getAll(page, size, includeStudents, includeTaskResults);
+        var modelList = modelService.getAll(pageable, includeStudents, includeTaskResults);
         var modelWithPersonDTOList = modelList.stream()
                 .map(model -> ModelWithPersonDTOMapper.makeDTO(model, PersonDTOMapper.makeDTO(model.getPerson())))
                 .toList();
@@ -74,7 +83,7 @@ public class ModelController {
     @GetMapping(BY_ID)
     public ResponseEntity<ModelDetailDTO> getModelDetailById(@PathVariable Long id) {
         var modelDetailDTO = modelService.getById(id)
-                .map(modelService::getModelDetailDTOByModel)
+                .map(modelEntityRelationFacade::getModelDetailDTOByModel)
                 .orElseThrow(() -> new ModelNotFoundException("Model with id=%d was not found!".formatted(id)));
         return ResponseEntity.ok(modelDetailDTO);
     }
@@ -85,7 +94,7 @@ public class ModelController {
                 .orElseThrow(() -> new PersonNotFoundException("Person with id=%d was not found".formatted(personId)));
         var modelList = modelService.getAllByPerson(person);
         var modelWithPersonDTOList = modelList.stream()
-                .map(model -> ModelWithPersonDTOMapper.makeDTO(model,PersonDTOMapper.makeDTO(model.getPerson())))
+                .map(model -> ModelWithPersonDTOMapper.makeDTO(model, PersonDTOMapper.makeDTO(model.getPerson())))
                 .toList();
         return ResponseEntity.ok(modelWithPersonDTOList);
     }
@@ -107,14 +116,17 @@ public class ModelController {
                 .toList();
         var task = taskId != null
                 ? taskService.getByIdWithResults(taskId)
-                .orElseThrow(() -> new TaskNotFoundException("Task with id=%d was not found".formatted(taskId)))
+                    .orElseThrow(() -> new TaskNotFoundException("Task with id=%d was not found".formatted(taskId)))
                 : null;
         modelDTO.setIsTaskResult(task != null);
         try {
-            var model = modelService.create(modelDTO, modelEntityDTOList, relationDTOList, person);
+            var model =
+                    modelEntityRelationFacade.createModelWithRelatedEntities(modelDTO, person,
+                            modelEntityDTOList, relationDTOList);
             if (task != null) resultService.sendResult(model, task);
             return new ResponseEntity<>(Map.of("modelId", model.getId()), HttpStatus.CREATED);
-        } catch (ModelCreationException | ResultCreationException exception) {
+        } catch (ModelCreationException | ModelEntityCreationException |
+                 RelationCreationException | ResultCreationException exception) {
             throw new ModelServerException(exception.getMessage(), exception);
         }
     }
@@ -124,9 +136,9 @@ public class ModelController {
         var model = modelService.getById(id)
                 .orElseThrow(() -> new ModelNotFoundException("Model with id=%d was not found!".formatted(id)));
         try {
-            modelService.deleteModel(model);
+            modelEntityRelationFacade.deleteModelWithRelations(model);
             return ResponseEntity.noContent().build();
-        } catch (ModelDeleteException exception) {
+        } catch (ModelDeleteException | RelationDeleteException exception) {
             throw new ModelServerException(exception.getMessage(), exception);
         }
     }
